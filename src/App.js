2095452fde328e04561a2863685e813ba5fa058b
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-10-29 00:07:27 +0300

    confirmed my apllication  is working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import './App.css';
+import Home from './components/Home/home';
 
-function App() {
+function App ( )  {
   return (
-    <div>
-    </div>
-  );
-}
+      <div className='App'>
+          <Home />
+      </div>
 
-export default App;
+  )
+}
+export default App
---
+++ src/components/Home/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.css"
+
+const Home = ( ) => {
+    return (
+        <div className="bar">
+            <h1>WELCOME TO MY REACT APP</h1>
+        </div>
+    )
+    }
+
+export default Home
