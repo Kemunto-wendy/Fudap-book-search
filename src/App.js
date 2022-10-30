79131d76ff063bced86b8bb9c8056c195b557956
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-10-30 08:28:56 +0300

    deleted irrelevant components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Form } from 'react-router-dom';
 import './App.css';
-import Book from './components/Book/book';
 import Display from './components/Display/display';
 import Home from './components/Home/home';
 
--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import "./home.css"
 import THREE from "../../assets/four.jpg"
 
+
 const Home = ( ) => {
+
     return (
         <div className="bar">
             <h1>ARE YOU A BOOK LOVER !</h1>
             <img src={THREE}  alt=""/>
             <p>“If you only read the books that everyone else is reading, you can only think what everyone else is thinking.”
             ― Haruki Murakami, Norwegian Wood</p>
+            <div className="bar">
+            <input type="text" placeholder="Search Book" />
+                        </div>
             </div>
     )
     }
